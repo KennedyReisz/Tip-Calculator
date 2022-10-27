9ac10dcad6697474cec040a53b1d4564f67ffb56
Kennedy <zelpzxhacker@gmail.com>
2022-10-27 10:56:25 -0300

    Finished LeftSide frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,45 @@
+// Import's
+import { FaUserAlt } from "react-icons/fa";
+
+// Asset's
 import styles from "../styles/Styles.module.css";
 
 const App = () => {
   return (
     <div className={styles.page}>
       <div className={styles.container}>
-        <div className={styles.leftSide}></div>
+        <div className={styles.leftSide}>
+          <div className={styles.billArea}>
+            <h2>Bill</h2>
+
+            <div className={styles.billInput}>
+              <p>$</p>
+              <input type="number" />
+            </div>
+          </div>
+
+          <div className={styles.selectTip}>
+            <h2>Select Tip %</h2>
+
+            <div className={styles.tipsPct}>
+              <button>5%</button>
+              <button>10%</button>
+              <button>15%</button>
+              <button>25%</button>
+              <button>50%</button>
+              <input type="number" placeholder="Custom" />
+            </div>
+          </div>
+
+          <div className={styles.peopleQuantity}>
+            <h2>Number of People</h2>
+
+            <div className={styles.peopleInput}>
+              <FaUserAlt color="#acbebc" />
+              <input type="number" />
+            </div>
+          </div>
+        </div>
         <div className={styles.rightSide}></div>
       </div>
     </div>
